a49bdbdd87feb4c39e750aeab0e9c95d5fa54563
Stewart Bonnick <stewart.bonnick@autoclavestudios.com>
2019-05-26 21:55:04 -0400

    split eslint tests per file

--- test/eslint.test.js
+++ test/eslint.test.js
@@ -3,11 +3,16 @@ const lint = require('mocha-eslint')
 var options = {
   formatter: 'compact',
   alwaysWarn: false, 
-  timeout: 5000, 
+  timeout: 10000, 
   slow: 1000,
   strict: true, 
   contextName: 'eslint'
 }
 
+var paths = [
+  'src/**/*.js',
+  'test/**/*.js'
+]
+
 // Run the tests
-lint('.', options)
+lint(paths, options)
