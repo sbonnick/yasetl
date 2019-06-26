1a4e6c35066d25a6cd967e9d6ff38c5febf162b5
Stewart Bonnick <stewart.bonnick@autoclavestudios.com>
2019-06-25 21:58:35 -0400

    Added tests for Passthrough Writer class

---
+++ test/src/plugins/writers/passthrough.test.js
@@ -0,0 +1,34 @@
+const Passthrough = require('../../../../src/plugins/writers/passthrough')
+const chai = require('chai')
+const chaiPromise = require('chai-as-promised')
+
+chai.use(chaiPromise)
+const expect = chai.expect
+
+describe('Writer Passthrough', () => {
+  describe('items()', () => {
+    it('will return the items on the config when passed through', async function () {
+      let items = ['row one', 'row two']
+      let passthrough = new Passthrough({})
+      passthrough.items(items, {})
+      expect(passthrough.config.items).to.be.eql(items)
+    })
+
+    it('will return the items on the config when passed through and joined with existing configs', async function () {
+      let initConfig = ['config one']
+      let items = ['row one', 'row two']
+      let passthrough = new Passthrough({ items: initConfig })
+      passthrough.items(items, {})
+      expect(passthrough.config.items).to.be.eql([...initConfig, ...items])
+    })
+
+    it('will return the items on the config when passed through and joined with override configs', async function () {
+      let initConfig = ['config one']
+      let items = ['row one', 'row two']
+      let overrideConfig = ['config two']
+      let passthrough = new Passthrough({ items: initConfig })
+      passthrough.items(items, { items: overrideConfig })
+      expect(passthrough.config.items).to.be.eql([...overrideConfig, ...items])
+    })
+  })
+})
